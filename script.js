aac484a51b9e632ed0fa55e2bcb3aed8fd656a45
Urvashi021 <127461389+Urvashi021@users.noreply.github.com>
2023-05-20 20:40:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onscroll= function() {
+    let winScroll = document.documentElement.scrollTop;
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrolled = (winScroll/height) * 100;
+
+    document.getElementById("myBar").style.width=scrolled + "%";
+}
